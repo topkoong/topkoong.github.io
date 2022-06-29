63aaa440624737b4b6e646fee4289a13bf65b513
toppy <theerut@shoplive.dev>
2022-06-29 23:37:07 +0700

    Fixed next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '',
   },
-  assetPrefix: isProd ? '/topkoong.github.io/' : '',
 };
 
 module.exports = nextConfig;
