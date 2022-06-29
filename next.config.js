fafbcc3ca138fa5c324a99bb1abd40a72b202830
toppy <theerut@shoplive.dev>
2022-06-29 23:24:21 +0700

    Updated next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   assetPrefix: isProd ? '/topkoong.github.io/' : '',
 };
 
