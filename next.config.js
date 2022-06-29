0b34fd67c22da5331a9b2295041f5b7d7cdaa998
toppy <theerut@shoplive.dev>
2022-06-29 22:26:46 +0700

    Updated deployment config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? '/topkoong.github.io/' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
